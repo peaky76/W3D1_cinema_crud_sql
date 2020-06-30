9d230120a6a337a33b979cb301f0fa6e4717a15c
peaky76 <robertjamespeacock@gmail.com>
2020-06-30 09:37:06 +0100

    corrections made following solution

--- marvel.sql
+++ marvel.sql
@@ -57,7 +57,7 @@ SELECT * FROM movies;
 SELECT name FROM people;
 
 -- 3.  Oops! Someone spelled Krusty The Clown's name wrong! Change it to reflect the proper spelling (Crusty should be Krusty).
-UPDATE people SET name = 'Krusty the Clown' WHERE name = 'Crusty the Clown';
+UPDATE people SET name = 'Krusty the Clown' WHERE id = 13;
 
 -- 4.  Return ONLY Homer Simpson's name from the 'people' table.
 SELECT name FROM people WHERE name = 'Homer Simpson';
@@ -76,11 +76,11 @@ INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 20
 
 -- 9.  The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
 SELECT show_time FROM movies WHERE title = 'Iron Man 2';
-INSERT INTO movies (title, year, show_time) VALUES ('Iron Man 3', 2013, '20:45');
+UPDATE movies SET show_time = '20:45' WHERE title = 'Iron Man 3';
 
 -- Extension 1.  Research how to delete multiple entries from your table in a single command.
 DELETE FROM movies WHERE title IN ('Thor', 'Iron Man', 'The Incredible Hulk');
 DELETE FROM people WHERE name LIKE '%Simpson';
 
--- SELECT * FROM people;
--- SELECT * FROM movies;
+SELECT * FROM people;
+SELECT * FROM movies;
